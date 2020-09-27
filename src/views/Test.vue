68d573a08cab11ebc7cdcb1fea6ed1f01f6e566d
dlcincluded <carydavid85@gmail.com>
2020-09-27 10:14:29 -0400

    refactoring base project
    just a base project, fixing an issue with previous setup

---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>TEST</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  color: blue;
+  h1 {
+    text-decoration: underline;
+  }
+}
+</style>
