bd421fbd6b481c2252ba72c9f618d36362e9d2cb
dlcincluded <carydavid85@gmail.com>
2020-09-27 21:08:10 -0400

    starting on workout page

--- src/router/index.js
+++ src/router/index.js
@@ -16,12 +16,12 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/test',
-    name: 'Test',
+    path: '/workout',
+    name: 'Workouts',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Test.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Workout.vue')
   }
 ]
 
---
+++ src/views/Workout.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="workout-container">
+    <div class="workout-container__workout-box" v-for="(workoutday,index) in workout" v-bind:key="index">
+      {{workoutday.name}}
+      <div class="workout-container__workout-box__sets">
+        <ul>
+          <li v-for="(reps,index) in workoutday.sets" v-bind:key="index">
+            {{ reps }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    {{ dayOfWeek }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      workout: [
+        {
+          name: "bench",
+          sets: [
+            12, 12, 12
+          ]
+        },
+        {
+          name: "chest-fly",
+          sets: [
+            15, 12, 10
+          ]
+        }
+      ]
+    }
+  },
+  computed: {
+    dayOfWeek(){
+      const currentDate = new Date();
+      const currentDay = currentDate.getDay();
+      const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+      return weekday[currentDay]
+    }
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.workout-container {
+  display: flex;
+  flex-direction: column;
+
+  &__workout-box {
+    background-color: #9D9D9D;
+    padding:10px;
+    margin:10px auto;
+    width: 30%;
+  }
+}
+</style>
